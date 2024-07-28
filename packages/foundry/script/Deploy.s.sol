3b7647425fdae968b6fe17cd91e169ed71a7e2c2
Ahmed Borwin <ahmedborwin01@gmail.com>
2024-07-28 07:39:37 +0100

    unable to write abis

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/svgNFT.sol";
+import "../contracts/SVGNFT.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
-    SVGNFT nftContract;
 
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
@@ -15,9 +14,10 @@ contract DeployScript is ScaffoldETHDeploy {
                 "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
             );
         }
+
         vm.startBroadcast(deployerPrivateKey);
 
-        nftContract = new SVGNFT();
+        SVGNFT nftContract = new SVGNFT();
         console.logString(
             string.concat(
                 "The new SVG Contract deployed at: ",
@@ -27,16 +27,8 @@ contract DeployScript is ScaffoldETHDeploy {
 
         uint256 nftPrice = nftContract.price();
 
-        mintNFT(nftPrice);
-
-        nftContract = new SVGNFT();
-
-        Deployment memory newContractDeployed = Deployment(
-            "SVGNFT",
-            address(nftContract)
-        );
-
-        deployments.push(newContractDeployed);
+        uint256 tokenID = nftContract.mintItem{value: nftPrice}();
+        console.log("@@@tokenID", tokenID);
 
         vm.stopBroadcast();
 
@@ -46,10 +38,5 @@ contract DeployScript is ScaffoldETHDeploy {
          * This function should be called last.
          */
         exportDeployments();
-    }
-
-    function mintNFT(uint256 _nftPrice) public {
-        uint256 tokenID = nftContract.mintItem{value: _nftPrice}();
-        console.log("@@@tokenID", tokenID);
     }
 }
